0db7c3a50b8701ac7a4f7b3b4b28531172599243
opanopan <o_pan@mail.ru>
2017-04-07 15:05:10 +0300

    2

--- scr.sh
+++ scr.sh
@@ -2,8 +2,8 @@
 
 for i in {1..1000} 
 do
-    content=$(curl -u o_pan:Djhrcnfnbjy78 -s -b 1 -c 1 "http://storage1.local:8081/rest/issue/ADM-$i" |    xpath '/issue/field[@name="State"]/value/text()' 2>/dev/null)
-    name=$(curl -u o_pan:Djhrcnfnbjy78 -s -b 1 -c 1 "http://storage1.local:8081/rest/issue/ADM-$i" |    xpath '/issue/field[@name="updaterName"]/value/text()' 2>/dev/null)
+    content=$(curl -u o_pan:xxxx -s -b 1 -c 1 "http://xxxx:8081/rest/issue/ADM-$i" |    xpath '/issue/field[@name="State"]/value/text()' 2>/dev/null)
+    name=$(curl -u o_pan:xxxx -s -b 1 -c 1 "http://xxxx.local:8081/rest/issue/ADM-$i" |    xpath '/issue/field[@name="updaterName"]/value/text()' 2>/dev/null)
     if [ -n "$content" ]
     then
      echo -en "RTGT-$i\t$content\t$name\n" >> list
